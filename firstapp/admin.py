810fc021de2c66cfbdb79b38ab5c65458fc76109
abeishenaliev7 <81817535+abeishenaliev7@users.noreply.github.com>
2021-06-21 16:21:52 +0600

    Delete firstapp directory

--- firstapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from firstapp.models import *
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Review)
